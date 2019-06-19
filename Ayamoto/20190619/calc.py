fff6ef005c06eabcf4a4fe9d9376c4df78ab244d
AyamotoKohei <koh14taka2@gmail.com>
2019-06-19 18:43:53 +0900

    最初の勉強会

---
+++ Ayamoto/20190619/calc.py
@@ -0,0 +1,12 @@
+# add
+print(1+1)
+# sub
+print(3-1)
+# mul
+print(3*5)
+# div
+print(15/3)
+# mod
+print(10%4)
+# div
+print(9//3)
---
+++ Ayamoto/20190619/if.py
@@ -0,0 +1,8 @@
+var = 0
+
+if var == 1:
+    print("One")
+elif var == 0:
+    print("Zero")
+else:
+    print("Not One")
