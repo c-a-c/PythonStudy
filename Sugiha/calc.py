67b6e88a5a0ada530d9c4f0a8a7144592b6aef40
杉橋　真輝 <kaba-temple@kaba-temple.local>
2019-06-19 18:34:49 +0900

    最初の勉強会

---
+++ Sugiha/calc.py
@@ -0,0 +1,12 @@
+#add
+print(1+1)
+#sub
+print(1-1)
+#mul
+print(1*2)
+#div
+print(1/2)
+#div
+print(6 % 4)
+#mod"
+print(1//2)
---
+++ Sugiha/fizzbuzz.py
@@ -0,0 +1,10 @@
+print("Input number: ")
+for i in range(1,50):
+	if i % 15 == 0:
+		print("fizzbuzz")
+	elif i % 5 == 0:
+		print("buzz")
+	elif i % 3 == 0:
+		print("fizz")
+	else:
+		print(i)
