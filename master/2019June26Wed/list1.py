f4fda75265507a12837d81daef1e01e474e8ed12
Miyajima Kenta <shadowlink0122@gmail.com>
2019-06-26 18:46:01 +0900

    list and input

--- master/2019June26Wed/list1.py
+++ master/2019June26Wed/list1.py
@@ -1,19 +1,20 @@
 integer = [1,2,3,4,5,6,7,8,9,]
 
-# Use Range Function
-print("Use Range Function")
-for i in range(len(integer)):
-	print(integer[i], end=", ")
-print("\n")
+# # Use Range Function
+# print("Use Range Function")
+# for i in range(len(integer)):
+# 	print(integer[i], end=", ")
+# print("\n")
 
 # Get One Element
 print("Get One Element")
+
 for i in integer:
 	print(i, end=", ")
 print("\n")
 
-# Print List
-print("Print List")
-print(integer)
+# # Print List
+# print("Print List")
+# print(integer)
 
 
