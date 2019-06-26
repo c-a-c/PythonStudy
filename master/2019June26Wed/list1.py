f78993f8bc9be61a8a941180786162db8221afeb
Miyajima Kenta <shadowlink0122@gmail.com>
2019-06-26 16:40:45 +0900

    2019June26Web 資料

---
+++ master/2019June26Wed/list1.py
@@ -0,0 +1,19 @@
+integer = [1,2,3,4,5,6,7,8,9,]
+
+# Use Range Function
+print("Use Range Function")
+for i in range(len(integer)):
+	print(integer[i], end=", ")
+print("\n")
+
+# Get One Element
+print("Get One Element")
+for i in integer:
+	print(i, end=", ")
+print("\n")
+
+# Print List
+print("Print List")
+print(integer)
+
+
